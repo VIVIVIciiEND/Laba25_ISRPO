e8cda1e058103ba7e3e009dbd4655d0999487768
VIVIVIciiEND <akirmandinara@gmail.com>
2026-05-01 23:05:25 +0300

    fetch API implementation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { greet , add ,PI  } from "./utils";
+// import { greet , add ,PI  } from "./utils";
 // console.log("\n=== Деструктуризация массивов ===");
 // const colors = ["red" , "green" , "blue"];
 // const color1 = colors[0];
@@ -96,75 +96,195 @@ import { greet , add ,PI  } from "./utils";
 // console.log(cube(1));
 // console.log(E);
 
-console.log("промисы");
-const simplePromise = new Promise((resolve , reject) =>{
-    const success = true ;
-    if(success){
-        resolve("операция выполнена успешно");
-    } else {
-        reject("произошла ошибка");
-    }
-});
-simplePromise
-.then((result) => console.log("результат" , result))
-.catch((error) => console.log("ошибка" , error ));
-function delay(ms){
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve(`прошло ${ms} миллисекунд`);
-        } , ms);
-    });
-}
-delay(1000)
-.then((message) => console.log(message));
-function fetchUserData(userId){
-    return new Promise((resolve , reject) => {
-        setTimeout(()=>{
-            if(userId >0){
-                resolve ({
-                    id : userId , 
-                    name: "иван Иванов" , 
-                    email: "ivan@exaple.com",
-                });
-            }else {
-                reject("неверный id пользователя");
-            }
-        }, 1500);
-    })
-}
-fetchUserData(1)
-.then ((user) => console.log("пользователь" , user))
-.catch((error) => console.log("ошибка", error));
-function step1(){
-    return new Promise((resolve) =>{
-        setTimeout(() => resolve("шаг 1 заверше") , 500);
-    });
-}
-function step2(previousResult){
-    return new Promise((resolve) => {
-        setTimeout(()=> resolve(`${previousResult} -> шаг 2 завершен`) , 500);
-    });
-}
-function step3(previousResult){
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(`${previousResult} -> шаг 3 завершен`) , 500);
-    });
-}
-step1()
-.then((result1) => step2(result1))
-.then((result2) => step3(result2))
-.then((finalResult) => console.log("финальный результат" , finalResult))
-.catch((error) => console.log("ошибка в цепочке" , error));
+// console.log("промисы");
+// const simplePromise = new Promise((resolve , reject) =>{
+//     const success = true ;
+//     if(success){
+//         resolve("операция выполнена успешно");
+//     } else {
+//         reject("произошла ошибка");
+//     }
+// });
+// simplePromise
+// .then((result) => console.log("результат" , result))
+// .catch((error) => console.log("ошибка" , error ));
+// function delay(ms){
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             resolve(`прошло ${ms} миллисекунд`);
+//         } , ms);
+//     });
+// }
+// delay(1000)
+// .then((message) => console.log(message));
+// function fetchUserData(userId){
+//     return new Promise((resolve , reject) => {
+//         setTimeout(()=>{
+//             if(userId >0){
+//                 resolve ({
+//                     id : userId , 
+//                     name: "иван Иванов" , 
+//                     email: "ivan@exaple.com",
+//                 });
+//             }else {
+//                 reject("неверный id пользователя");
+//             }
+//         }, 1500);
+//     })
+// }
+// fetchUserData(1)
+// .then ((user) => console.log("пользователь" , user))
+// .catch((error) => console.log("ошибка", error));
+// function step1(){
+//     return new Promise((resolve) =>{
+//         setTimeout(() => resolve("шаг 1 заверше") , 500);
+//     });
+// }
+// function step2(previousResult){
+//     return new Promise((resolve) => {
+//         setTimeout(()=> resolve(`${previousResult} -> шаг 2 завершен`) , 500);
+//     });
+// }
+// function step3(previousResult){
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(`${previousResult} -> шаг 3 завершен`) , 500);
+//     });
+// }
+// step1()
+// .then((result1) => step2(result1))
+// .then((result2) => step3(result2))
+// .then((finalResult) => console.log("финальный результат" , finalResult))
+// .catch((error) => console.log("ошибка в цепочке" , error));
 
-function checkInventory(Name) {
-    return new Promise((resolve, reject) => {
-        const inventory = ["Телефон", "Ноутбук", "Планшет"];
-        setTimeout(() => {
-            if (inventory.includes(Name)) {
-                resolve(`Товар ${Name} в наличии`);
-            } else {
-                reject(`Товар ${Name} не найден`);
-            }
-        }, 1000);
-    });
+// function checkInventory(Name) {
+//     return new Promise((resolve, reject) => {
+//         const inventory = ["Телефон", "Ноутбук", "Планшет"];
+//         setTimeout(() => {
+//             if (inventory.includes(Name)) {
+//                 resolve(`Товар ${Name} в наличии`);
+//             } else {
+//                 reject(`Товар ${Name} не найден`);
+//             }
+//         }, 1000);
+//     });
+// }
+
+// console.log("Async/Await");
+// async function greet(){
+//     return "Привет!";
+// }
+// greet().then((message) => console.log(message));
+// function getWeather(){
+//     return new Promise((resolve) => {
+//         setTimeout(()=>{
+//             resolve({temp: 22 , condition : "Солнечно"});
+//         } , 1000);
+//     })
+// }
+// async function showWeather(){
+//     console.log("загрузка погоды...");
+//     const weather = await getWeather();
+//     console.log(`температура ${weather.temp} , ${weather.condition}`);
+// }
+// showWeather();
+
+
+// async function fetchData(shouldFail){
+//     return new Promise((resolve , reject) => {
+//         setTimeout(()=>{
+//             if(shouldFail){
+//                 reject("ошибка при загрузке данных");
+//             }else{
+//                 resolve({data : "важные данные"});
+//             }
+//         } , 800);
+//     });
+// }
+// async function getData(){
+//     try{
+//         const result = await fetchData(false) ; 
+//         console.log("Успешно" , result.data) ; 
+//         const failedResult = await fetchData(true) ; 
+//         console.log("это не выполнится");
+//     }catch(error){
+//         console.log("поймана ошибка" , error);
+//     }
+// }
+// getData();
+
+// async function cookDinner(){
+//     console.log("начинаем готовить ...");
+//     const pasta = await delay(1000).then(() => "паста готова");
+//     console.log(pasta) ; 
+//     const sauce = await delay(500).then(() => "соус готов") ;
+//     console.log(sauce) ; 
+//     const salad = await delay(700).then(() => "салат гтов");
+//     console.log(salad);
+//     return "ужин готов";
+// }
+// cookDinner().then((result) => console.log(result));
+// async function cookDinnerFast(){
+//     console.log("готовим все одноврменно...");
+//     const [pasta , sauce, salad] = await Promise.all([
+//         delay(1000).then(() => "паста готова"), 
+//         delay(500).then(() => "соус готов") , 
+//         delay(700).then(() => "салат  готов"), 
+//     ]); 
+//     console.log(pasta , sauce , salad) ;
+//     return "ужин готов быстрее"; 
+// }
+// cookDinnerFast().then((result) => console.log(result));
+// console.log("Fetch API");
+// async function getUsers(){
+//     try{
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//         if(!response.ok){
+//             throw new Error(`HTTP ошибка Статус ${response.status}`);
+//         }
+//         const users = await response.json();
+//         console.log("первые 3 пользователя");
+//         users.slice(0,3).forEach((user) => {
+//             console.log(`-${user.name} (${user.email})`);
+//         });
+//     } catch (error){
+//         console.log("ошибка при загрузке пользователей" , error.message) ; 
+//     }
+// }
+// getUsers();
+
+// async function getUserById(id){
+//     try{
+//         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+//         const user = await response.json();
+//         console.log(`пользователь ${id}`);
+//         console.log(`имя ${user.name}`);
+//         console.log(`город ${user.address.city}`);
+//         console.log(`компания ${user.company.name}`);
+//     }catch(error){
+//         console.log("ошибка" , error.message) ; 
+//     }
+// }
+// getUserById(1);
+async function createPost(){
+    try{
+        const newPost={
+            title : "моя первая запись" , 
+            body : "это содержание моей первйо записи в блоге" , 
+            userId: 1 , 
+        };
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts" , {
+            method: "POST" ,
+            headers: {
+                "Content-Type" : "application/json" , 
+            },
+            body : JSON.stringify(newPost) , 
+        });
+        const createPost = await response.json();
+        console.log("создана новая запись"); 
+        console.log("ID" , createPost.id) ; 
+        console.log("заголовок" , createPost.title) ; 
+    }catch(error){
+        console.log("ошибка при создании записи" , error.message) ; 
+    }
 }
+createPost();
